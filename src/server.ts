139b32bccf9017bce935e5cef13d45235fdbb18b
jaydenteo <jayden228@gmail.com>
2024-11-02 15:49:35 +0800

    connect mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
-// Import the 'express' module along with 'Request' and 'Response' types from express
-import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './db/connect';
 
-// Create an Express application
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 5000;
 
-// Specify the port number for the server
-const port: number = 3000;
+const start = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is not defined in environment variables');
+    }
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch(error) {
+    console.log(error);
+  }
+}
 
-// Define a route for the root path ('/')
-app.get('/', (req: Request, res: Response) => {
-  // Send a response to the client
-  res.send('Hello, TypeScript + Node.js + Express!');
-});
-
-// Start the server and listen on the specified port
-app.listen(port, () => {
-  // Log a message when the server is successfully running
-  console.log(`Server is running on http://localhost:${port}`);
-});
+start();
